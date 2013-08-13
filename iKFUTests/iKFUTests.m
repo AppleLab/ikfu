ad7dc6fd603e4a927962ab68535ebd099d71b695
Ramil Garaev <ramilgaraev@libr-3.i.ksu.ru>
2013-08-12 21:06:40 -0700

    Initial Commit

---
+++ iKFUTests/iKFUTests.m
@@ -0,0 +1,34 @@
+//
+//  iKFUTests.m
+//  iKFUTests
+//
+//  Created by Ramil Garaev on 12.08.13.
+//  Copyright (c) 2013 Ramil Garaev. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iKFUTests : XCTestCase
+
+@end
+
+@implementation iKFUTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
