2b083a8bd13880ae23122177ff92af4ec8d88aeb
user <user@Mac-mini-administartor.local>
2013-08-15 17:35:06 +0400

    Добавили возможность регистрации VK

--- iKFU/VkViewController.h
+++ iKFU/VkViewController.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface VkViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIWebView *web;
 
 @end
--- iKFU/VkViewController.m
+++ iKFU/VkViewController.m
@@ -13,7 +13,7 @@
 @end
 
 @implementation VkViewController
-
+@synthesize web;
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -22,11 +22,12 @@
     }
     return self;
 }
-
+//
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	
+	[web loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://oauth.vk.com/authorize?client_id=3822741&redirect_uri=http://oauth.vk.com/blank.html&display=touch&response_type=token&scope=offline"]]];
+//    [web setScalesPageToFit:YES];
 }
 
 - (void)didReceiveMemoryWarning
