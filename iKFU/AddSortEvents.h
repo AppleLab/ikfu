c80b0aae6dc469c9830f1f80a2e30b7e52ba439d
user <user@Mac-mini-administrator-9.local>
2013-08-16 16:26:41 +0400

    Класс который получает данные с сервера и парсит их.

---
+++ iKFU/AddSortEvents.h
@@ -0,0 +1,13 @@
+//
+//  AddSortEvents.h
+//  iKFU
+//
+//  Created by user on 16.08.13.
+//  Copyright (c) 2013 Ramil Garaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddSortEvents : UIViewController
+
+@end
