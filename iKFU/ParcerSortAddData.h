fb8c5118724b66af9cf02560b6ab52c829684f0c
user <user@Mac-mini-administrator-9.local>
2013-08-16 20:59:36 +0400

    Работает парсер, при нажатие кнопки «все мероприятия » отображает все мероприятия, остальные разделы не работают.

---
+++ iKFU/ParcerSortAddData.h
@@ -0,0 +1,19 @@
+//
+//  ParcerSortAddData.h
+//  iKFU
+//
+//  Created by user on 16.08.13.
+//  Copyright (c) 2013 Ramil Garaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ParcerSortAddData : UITableViewController
+//<UITableViewDelegate, UITableViewDataSource>
+
+//@property (strong, nonatomic) IBOutlet UITableView *newsList;
+@property (strong, nonatomic) NSArray *news;
+
+
+
+@end
