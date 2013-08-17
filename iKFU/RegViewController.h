a3dec75d5d0c08525d530233f472180ef4a2cd57
user <user@Mac-mini-administrator-66.local>
2013-08-17 17:57:07 +0400

    Обновленная версия входа в ЛК, ВК, FB.

---
+++ iKFU/RegViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegViewController.h
+//  iKFU
+//
+//  Created by user on 17.08.13.
+//  Copyright (c) 2013 Ramil Garaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegViewController : UIViewController
+
+@end
---
+++ iKFU/RegViewController.m
@@ -0,0 +1,42 @@
+//
+//  RegViewController.m
+//  iKFU
+//
+//  Created by user on 17.08.13.
+//  Copyright (c) 2013 Ramil Garaev. All rights reserved.
+//
+
+#import "RegViewController.h"
+
+@interface RegViewController ()
+
+@end
+
+@implementation RegViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	/*if([[NSUserDefaults standardUserDefaults]objectForKey:@"vk_token"] != nil){
+        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+        UIViewController *yourViewController = (UIViewController *)[storyboard instantiateViewControllerWithIdentifier:@"TabBarController"];
+        [self presentModalViewController:yourViewController animated:YES];
+    }*/
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
