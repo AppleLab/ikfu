10ea2b230b32c779170adee2a709ef43108554c0
user <user@Mac-mini-administrator-7.local>
2013-08-23 14:00:57 +0400

    bh

---
+++ iKFU/List.h
@@ -0,0 +1,20 @@
+
+//
+// ProfileViewController.h
+// iKFU
+//
+// Created by user on 17.08.13.
+// Copyright (c) 2013 Ramil Garaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface List : UITableViewController{
+    
+    NSArray *list;
+    
+}
+
+@property (nonatomic, retain) NSArray *list;
+
+@end
